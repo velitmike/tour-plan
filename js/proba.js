c9b2c6d5c060e52fbd71e7e9a1c55929bb24985d
velitmike <mvelitchenko@yandex.ru>
2020-09-22 19:45:01 +1200

    create: added prace & map in hotel section

---
+++ js/proba.js
@@ -0,0 +1,6 @@
+console.log("Страница загрузилась");
+var button = document.querySelector("button");
+console.log(button);
+button.addEventListener("click", function(event){
+	console.log(event);
+});
